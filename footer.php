f4d056f3e05d8f1fc79b8e098ae5f03f29ee8046
shearusherly <42143374+shearusherly@users.noreply.github.com>
2018-12-10 12:55:15 +0530

    my  new commit

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@ echo "<p><i>All content on this site is &copy; "
 if(@$_SESSION['SESS_ADMINLOGGEDIN'] == 1)
 {
 echo "[<a href='" . $config_basedir . "adminorders.php'>admin</a>][<a href='". $config_basedir. "adminlogout.php'>admin logout</a>]";
+echo "hello";
 }
 ?>
 </div>
